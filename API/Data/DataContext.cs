ac83add203058901802120c4a13008bdb4f931c4
mcbalsa <mcbalsa@protonmail.com>
2024-03-18 15:43:26 -0400

    postgres connection

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using API;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+  
+
+   
+}
